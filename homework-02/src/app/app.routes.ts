5fe3e84b08f495637917f509dbb3af0bb6689fd1
ljupce111 <ljupcemadafaka@gmail.com>
2025-09-19 20:03:24 +0200

    homework 2 updated to homework 3 and homework 3 updated to homework 4

--- homework-02/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { AddTodoComponent } from '../add-todo/add-todo';
-import { Routes } from '@angular/router';
-import { ViewTodosComponent} from '../view-todos/view-todos';
-
-
-export const routes: Routes = [
-    { path:'', component: ViewTodosComponent},
-    { path:'addTodo', component: AddTodoComponent}
-];
